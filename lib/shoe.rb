c4c005128f0def1c39e30e915fb153e66627718c
Samuel Rudnicki <samuel.rudnicki@gmail.com>
2020-06-09 17:36:21 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,7 +1,7 @@
 class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
-  
+
   BRANDS = []
 
   def initialize(brand)
@@ -18,4 +18,3 @@ class Shoe
     BRANDS << brand
   end
 end
-
